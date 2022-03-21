f15922c2a38f0d41640503b2b5182cb55e091de7
naimur2 <naimurrahaman248@gmail.com>
2022-03-21 22:08:13 +0600

    updates

--- index.js
+++ index.js
@@ -2,21 +2,20 @@
 const express = require("express");
 const cors = require("cors");
 
+
 const app = express();
 
 const testRouter = require("./routers/testRouter");
 const inceptionRouter = require("./routers/inceptionRouter");
+
+
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/vgg", testRouter);
 
-
-
 app.use("/inception", inceptionRouter);
-
-
 
 const errorHandler = (err, req, res, next) => {
     if (req.headerSent) {
@@ -24,6 +23,7 @@ const errorHandler = (err, req, res, next) => {
     }
     return res.status(500).json({ error: err });
 };
+
 
 app.listen(4000, () => {
     console.log("App Started at port localhost:4000");
